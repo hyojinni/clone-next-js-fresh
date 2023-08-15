740ba4a05c0c4f8f8cf711d2bb89ffd783329c4f
hyojinni <hyojinni@gmail.com>
2023-08-15 22:47:36 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+    server: {
+        port: 2000,
+        host: '0.0.0.0',
+        timing: false
+    }
 }
 
 module.exports = nextConfig
